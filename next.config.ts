cfd5108669588f1ca39d51303da03491c31c1c73
Mannan88 <kocharmanan88@gmail.com>
2026-08-26 03:43:03 +0530

    workflow added

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   reactCompiler: true,
+
   images: {
     unoptimized: true,
   },
+
+  basePath: "/component-lib",
+  assetPrefix: "/component-lib/",
 };
 
 export default nextConfig;
